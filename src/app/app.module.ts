ed3d413471e749d25973255c2afbfbc7b50b7768
Ivan <ivanagustinperez@gmail.com>
2023-04-03 23:46:54 -0300

    3-4-2023

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,25 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { PresentacionComponent } from './presentacion/presentacion.component';
+import { AboutMeComponent } from './about-me/about-me.component';
+import { EstudiosComponent } from './estudios/estudios.component';
+import { ExperienciaComponent } from './experiencia/experiencia.component';
+import { ContactoComponent } from './contacto/contacto.component';
+
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    PresentacionComponent,
+    AboutMeComponent,
+    EstudiosComponent,
+    ExperienciaComponent,
+    ContactoComponent
   ],
   imports: [
     BrowserModule,
