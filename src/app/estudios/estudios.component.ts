2b866ec20bfea324a95936bfc494e8f269dca6bd
Ivan <ivanagustinperez@gmail.com>
2023-04-16 21:33:31 -0300

    16-04-2023

--- src/app/estudios/estudios.component.ts
+++ src/app/estudios/estudios.component.ts
@@ -8,3 +8,22 @@ import { Component } from '@angular/core';
 export class EstudiosComponent {
 
 }
+
+function efectoHabilidades(): void {
+  const skills: any = document.getElementById("soft-skills");
+  const distancia_skills = window.innerHeight - skills.getBoundingClientRect().top;
+  if (distancia_skills >= 300) {
+      const habilidades = document.getElementsByClassName("progreso");
+      habilidades[0].classList.add("trabajo");
+      habilidades[1].classList.add("creatividad");
+      habilidades[2].classList.add("comunicacion");
+      habilidades[3].classList.add("dedicacion");
+      habilidades[4].classList.add("liderazgo");
+      habilidades[5].classList.add("altruismo");
+  }
+}
+
+window.onscroll = function() {
+  efectoHabilidades();
+};
+
